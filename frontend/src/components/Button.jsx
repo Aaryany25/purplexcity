99dea0dfee6b07cc6273150473708a3d8393f9e5
Aryan Yadav <aaryany2505@gmail.com>
2026-05-12 11:20:56 +0530

    installed shadcn and react router dom

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Button({title}) {
+  return (
+    <button className='bg-white rounded py-2 px-2 '>{title}</button>
+  )
+}
+
+export default Button
---
+++ frontend/src/pages/Auth.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '../components/Button'
+
+function Auth() {
+  return (
+    <div className='flex'>
+     <button>SignIn with Google</button>
+     <button>SignIn with Github</button>
+
+    </div>
+  )
+}
+
+export default Auth
