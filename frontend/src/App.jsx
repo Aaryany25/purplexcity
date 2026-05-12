fad12b0e0cf5a5b1153c25eeed1645092bff3ddf
Aryan Yadav <aaryany2505@gmail.com>
2026-05-12 12:11:46 +0530

    OAuth done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Auth from './pages/Auth'
+import Home from './pages/Home'
+
 function App() {
   return (
     <div className='w-full h-screen'>
       <Routes>
+        <Route path="/" element={<Home/>}/>
+        
         <Route path="/auth" element={<Auth/>}/>
       </Routes>
     </div>
--- frontend/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Button({title}) {
-  return (
-    <button className='bg-white rounded py-2 px-2 '>{title}</button>
-  )
-}
-
-export default Button
